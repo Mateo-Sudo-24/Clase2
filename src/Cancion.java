38f9e4429f1282435cee2f4ac912b1f3e436183e
Mateo-Sudo-24 <mateo.paredes@epn.edu.ec>
2024-10-10 16:13:41 +0200

    Cambios clase cancion y videojuegos

--- src/Cancion.java
+++ src/Cancion.java
@@ -1,12 +1,12 @@
 public class Cancion {
-    String artista;
+    String nombre_cancion;
     String genero;
     int anio;
     int numero_de_canciones;
     int calificacion;
 
-    public Cancion(String artista, String genero, int anio, int numero_de_canciones, int ventas) {
-        this.artista = artista;
+    public Cancion(String nombre_cancion, String genero, int anio, int numero_de_canciones, int ventas) {
+        this.nombre_cancion = nombre_cancion;
         this.genero = genero;
         this.anio = anio;
         this.numero_de_canciones = numero_de_canciones;
@@ -14,7 +14,7 @@ public class Cancion {
     }
 
     public void imprimir_artista() {
-        System.out.println("Artista: " + artista);
+        System.out.println("Nombre de la cancion: " + nombre_cancion);
         System.out.println("Genero: " + genero);
         System.out.println("Anio: " + anio);
         System.out.println("Numero de Canciones: " + numero_de_canciones);
@@ -23,5 +23,25 @@ public class Cancion {
 
     public int canciones_escondidas() {
         return numero_de_canciones+2 ;
+    }
+
+    public String getNombre_cancion() {
+        return nombre_cancion;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public int getNumero_de_canciones() {
+        return numero_de_canciones;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public int getCalificacion() {
+        return calificacion;
     }
 }
